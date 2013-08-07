82e69a702c2a784e1d04a127a6cfb871c7cd6fd6
Road Tang <roadtang@gmail.com>
2013-08-07 11:00:56 +0800

    init repo, scrapy new project

---
+++ scrapybot/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for scrapybot project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'scrapybot'
+
+SPIDER_MODULES = ['scrapybot.spiders']
+NEWSPIDER_MODULE = 'scrapybot.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapybot (+http://www.yourdomain.com)'
