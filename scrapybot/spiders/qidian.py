f8119882d57365cf1a599179058c8a72af0494b8
Frank Tang <roadtang@gmail.com>
2016-05-02 21:59:51 +0800

    qidian - add nodeep, nonext argument

--- scrapybot/spiders/qidian.py
+++ scrapybot/spiders/qidian.py
@@ -14,6 +14,22 @@ logger = logging.getLogger(__name__)
 
 
 class QidianSpider(BaseSpider):
+    '''
+
+    Available arguments
+    download urls
+    - a url=xxx  -a urls=aaa 
+
+    use a callable to generate urls  (url generator may accept it own args. as following v='xxx')
+    -a  urlg=a.b.c.urls_to_down   -a  v=xxx
+    
+    don't scrap next level
+    -a nodeep=True 
+
+    only scrap one page
+    -a nonext=True
+
+    '''
     name = 'qidian'
     allowed_domains = ['qidian.com']
 
@@ -37,34 +53,38 @@ class QidianSpider(BaseSpider):
         kwargs = self.kwargs
 
         #if kwags is emtpy, route to old logic
-        if not kwargs:
-            return super(QidianSpider, self).start_requests(*args, **kwargs)
+        if 'url' in kwargs or 'urls' in kwargs or 'urlg' in kwargs:
             
-        # setup urls , and parse kwargs
-        urls = set()
+            # setup urls , and parse kwargs
+            urls = set()
         
-        # deal with url, urls param
-        param_url = kwargs.get('url')
-        param_urls = kwargs.get('urls')
-        param_urls = param_urls and param_urls.split(',') 
-        if param_url:
-             urls.add(param_url)
-        if param_urls:
-            urls = urls.union(param_urls)
+            # deal with url, urls param
+            param_url = kwargs.get('url')
+            param_urls = kwargs.get('urls')
+            param_urls = param_urls and param_urls.split(',') 
+            if param_url:
+                urls.add(param_url)
+            if param_urls:
+                urls = urls.union(param_urls)
 
     
-        # deeal wiht urlgen
-        param_urlg = kwargs.get('urlg')
-        if param_urlg:
-            urlg = load_object(param_urlg)
-            if callable(urlg):
-                urls = urls.union(urlg(**kwargs))
+            # deeal wiht urlgen
+            param_urlg = kwargs.get('urlg')
+            if param_urlg:
+                urlg = load_object(param_urlg)
+                if callable(urlg):
+                    urls = urls.union(urlg(**kwargs))
 
-        logger.debug("%s:%s %s" % (type(self).__name__,  'start_requests',  [kwargs, urls]))
-        for url in urls:
-            callback = self.callback_from_url(url)
-            if callback:
-                yield scrapy.Request(url, callback=callback)
+            logger.debug("%s:%s %s" % (type(self).__name__,  'start_requests',  [kwargs, urls]))
+            for url in urls:
+                callback = self.callback_from_url(url)
+                if callback:
+                    yield scrapy.Request(url, callback=callback)
+
+        else:
+            for r in super(QidianSpider, self).start_requests():
+                yield r
+
 
 
     def urls_from_generator(self, urlg, **kwargs):
@@ -117,7 +137,20 @@ class QidianSpider(BaseSpider):
             i['last_update'] = self.extract(h, 'div[@class="swe"]/text()')
             i['key'] = re.search("/(\d+).aspx$",i['url']).group(1)
             #Request(i['author_url'],callback=self.parse_user)
-            yield Request(i['url'], meta={'article':i},callback=self.parse_article_detail)
+            yield i
+
+            # fire to scrap detal page
+            if not 'nodeep' in self.kwargs:
+                yield Request(i['url'], meta={'article':i},callback=self.parse_article_detail)
+
+
+        # next gallery page
+        if not 'nonext' in self.kwargs:
+            next_page = response.xpath('//a[@class="f_a"]/@href').extract_first()
+            if next_page:
+                url = response.urljoin(next_page)
+                req =  scrapy.Request(url, callback=self.parse_article_list)
+                yield req
 
 
     def parse_user(self, response):
@@ -126,3 +159,4 @@ class QidianSpider(BaseSpider):
     def extract(self, hxs, xpath, defv=None):
         arr = hxs.select(xpath).extract()
         return arr and arr[0] or defv
+
