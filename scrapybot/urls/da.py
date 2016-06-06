39bc973c0dfbfb0134b0a5265a0df162ab0e1fc1
Frank Tang <roadtang@gmail.com>
2016-06-06 13:25:54 +0800

    urls - da -  a little rewrite (no logic change)

--- scrapybot/urls/da.py
+++ scrapybot/urls/da.py
@@ -2,16 +2,12 @@
 
 from . import mongodb
 
+
 def art(**kwargs):
     '''generate urls of art with s= (search text), u= (","-separated author_names), t= (","-separated tags) '''
-    with mongodb()  as db:
+    with mongodb() as db:
         cond = {}
-        if 's' in kwargs:
-            cond['$text'] = { '$search' : kwargs.get('s') }
-        if 'u' in kwargs:
-            cond['author_name'] = { '$in' : kwargs.get('u').split(',') }
-        if 't' in kwargs:
-            cond['tag_names'] = { '$in' : kwargs.get('t').split(',') }
+        _fill_cond(cond, kwargs)
         urls = map(lambda e: e['url'], db.art.find(cond, {'url': 1}))
         return { 'url': urls }
                 
@@ -20,13 +16,18 @@ def art_to_down(**kwargs):
     '''generate urls of art which not yet downloaded
     with s= (search text), u= (","-separated author_names), t= (","-separated tags) '''
 
-    with mongodb()  as db:
+    with mongodb() as db:
         cond = { "file_url" : {'$eq' :None}}
-        if 's' in kwargs:
-                cond['$text'] = { '$search' : kwargs.get('s') }
-        if 'u' in kwargs:
-                cond['author_name'] = { '$in' : kwargs.get('u').split(',') }
-        if 't' in kwargs:
-                cond['tag_names'] = { '$in' : kwargs.get('t').split(',') }
+        cond = _fill_cond(cond, kwargs)
         urls = map(lambda e: e['url'], db.art.find(cond, {'url': 1}))
         return { 'url' : urls}
+
+
+def _fill_cond(cond, kwargs):
+        if 's' in kwargs:
+            cond['$text'] = { '$search' : kwargs.get('s') }
+        if 'u' in kwargs:
+            cond['author_name'] = { '$in' : kwargs.get('u').split(',') }
+        if 't' in kwargs:
+            cond['tag_names'] = { '$in' : kwargs.get('t').split(',') }
+        return cond
